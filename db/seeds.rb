39ee1313b34dac259eb8ca4a58998c906187dcaa
Arthur Menard <arthurmenard12@gmail.com>
2019-11-13 16:21:02 +0100

    Yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+Restaurant.create(name: " Tripletta ", address: "Cours du Médoc, Bordeaux ", category: "italian")
+Restaurant.create(name: " Bistrot Régent ", address: "Rue Saint-Rémi, Bordeaux ", category: "french")
+Restaurant.create(name: " Fufu ", address: "Cours Portal, Bordeaux ", category: "japanese")
+Restaurant.create(name: " Bloempot ", address: "2 rue Pierre Michel, Lille ", category: "belgian")
+Restaurant.create(name: " Belleza ", address: "56 rue du Temple, Lille ", category: "italian")
+Restaurant.all.each do |p|
+  puts "Created #{p.name} "
+end
